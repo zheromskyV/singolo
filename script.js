13a61bc6a3bcc31e43151f26c06300b45f8fa309
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2020-03-21 22:26:55 +0300

    feat: add additional scripts for singolo dom

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-window.onload = function() {
-	addHeaderCkickHandler();
+"use strict";
 
+window.onload = function() {
+	addNavScrollHandler();
+
+	initSlider();
 	addSliderCkickHandler();
 
 	addPortfolioButtonsClickHandler();
@@ -11,34 +14,34 @@ window.onload = function() {
 
 // header
 
-const addHeaderCkickHandler = () => {
-	document.querySelector(".header-nav").addEventListener("click", (e) => {
-		if (e.target.classList.contains("header-nav-ref")) {
-			let clickedNav = e.target;
-			console.log(e.target);
-			removeActiveNavs();
-			activateClickedNav(clickedNav);
-		}
+const addNavScrollHandler = () => {
+	document.addEventListener("scroll", e => {
+		const curPos = window.scrollY;
+		const sections = document.querySelectorAll("section");
+		document.querySelectorAll(".anchor").forEach((el, index) => {
+			let delta = index !== 0 ? 95 : 0;
+			if ((sections[index].offsetTop - delta) <= curPos && (sections[index].offsetTop + sections[index].offsetHeight + delta) > curPos) {
+				document.querySelectorAll(".navigation li a").forEach(a => {
+					if (a.classList.contains("header-nav-ref")) {
+						a.classList.remove("active-nav");
+						if (el.getAttribute("id") === a.getAttribute("href").substring(1)) {
+							a.classList.add("active-nav");
+						}
+					}
+				});
+			}
+		});
 	});
-}
-
-const removeActiveNavs = () => {
-	let navs = document.querySelectorAll(".navigation li a");
-	navs.forEach(nav => {
-		nav.classList.remove("active-nav");
-	});
-} 
-
-const activateClickedNav = (nav) => {
-	nav.classList.add("active-nav");
 }
 
 // slider
 
 const addSliderCkickHandler = () => {
 	document.querySelector(".slider-block").addEventListener("click", (e) => {
-		if (e.target.classList.contains("slider-block-arrows")) {
-			changeSlide();
+		if (e.target.classList.contains("left-arrow")) {
+			slideLeft();
+		} else if (e.target.classList.contains("right-arrow")) {
+			slideRight();
 		} else if (e.target.classList.contains("slider-block-iphones")
 			|| e.target.classList.contains("iphone_off")
 			) {
@@ -47,38 +50,91 @@ const addSliderCkickHandler = () => {
 	});
 }
 
-const changeSlide = () => {
-	let slides = document.querySelectorAll(".slide");
-	slides.forEach(slide => {
-		if (slide.classList.contains("hidden-slide")) {
-			slide.classList.remove("hidden-slide");
-		} else {
-			slide.classList.add("hidden-slide");
-			let background = document.querySelectorAll(".slider-block")[0];
-			let arrows = document.querySelectorAll(".slider-block-arrows");
-			let bgColor = background.style["background-color"];
-			let bgBorderBottom = background.style["border-bottom"];
-			let arsColor;
-			slide.childNodes.forEach(child => {
-				if (child.classList) {
-					if (child.classList.contains("red")) {
-						bgColor = "#648BF0"; // blue
-						bgBorderBottom = "6px solid rgb(74, 118, 231)";
-						arsColor = "sepia(100%)";
-					}
-					else if (child.classList.contains("blue")) {
-						bgColor = "#f06c64"; // red
-						bgBorderBottom = "6px solid #ea676b";
-						arsColor = "none";
-					}
-				}
-			});
-			background.style["background-color"] = bgColor;
-			background.style["border-bottom"] = bgBorderBottom;
-			arrows.forEach(arrow => {
-				arrow.style["filter"] = arsColor;
-			});
+let slider = document.querySelector("#slider");
+const sliderContent = [];
+let slides = [];
+let size = 940;
+let step = 0;
+let slidingBlock = false;
+
+const initSlider = () => {
+	let s = document.querySelectorAll(".slide");
+	for (let i = 0; i < s.length; i++) {
+		sliderContent.push(s[i].innerHTML);
+		s[i].remove();
+	}
+	drawSlide();
+}
+
+const drawSlide = () => {
+	let div = document.createElement("div");
+	div.classList.add("slide");
+	div.innerHTML = sliderContent[step];
+	div.style.left = 0 + "px";
+	slider.appendChild(div);
+	if (step + 1 === sliderContent.length) {
+		step = 0;
+	} else {
+		step++;
+	}
+}
+
+const slideLeft = () => {
+	if (slidingBlock) return;
+	slidingBlock = true;
+	let slides2 = document.querySelectorAll(".slide");
+	let offset2 = 0;
+	slides2.forEach(slide => {
+		slide.style.left = offset2 * size - size + "px";
+		offset2++;
+	});
+	changeSlide(slides2[0]);
+	setTimeout(() => {
+		slides2[0].remove();
+		drawSlide();
+		slidingBlock = false;
+	}, 1000);
+}
+
+const slideRight = () => {
+	if (slidingBlock) return;
+	slidingBlock = true;
+	let slides2 = document.querySelectorAll(".slide");
+	let offset2 = 0;
+	slides2.forEach(slide => {
+		slide.style.left = offset2 * size + size + "px";
+		offset2++;
+	});
+	changeSlide(slides2[0]);
+	setTimeout(() => {
+		slides2[0].remove();
+		drawSlide();
+		slidingBlock = false;
+	}, 1000);
+}
+
+const changeSlide = (slide) => {
+	let background = document.querySelectorAll(".slider-block")[0];
+	let arrows = document.querySelectorAll(".slider-block-arrows");
+	let bgColor, bgBorderBottom, arsColor;
+	slide.childNodes.forEach(child => {
+		if (child.classList) {
+			if (child.classList.contains("red")) {
+				bgColor = "#648BF0"; // blue
+				bgBorderBottom = "6px solid rgb(74, 118, 231)";
+				arsColor = "sepia(100%)";
+			}
+			else if (child.classList.contains("blue")) {
+				bgColor = "#f06c64"; // red
+				bgBorderBottom = "6px solid #ea676b";
+				arsColor = "none";
+			}
 		}
+	});
+	background.style["background-color"] = bgColor;
+	background.style["border-bottom"] = bgBorderBottom;
+	arrows.forEach(arrow => {
+		arrow.style["filter"] = arsColor;
 	});
 }
 
@@ -191,5 +247,6 @@ const closeGetAQuoteMessage = () => {
 	formAnswer.forEach(a => {
 		a.classList.add("form-answer_hidden");
 	});
+	document.querySelector(".get-a-quote__form").reset();
 }
 
